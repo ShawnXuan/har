ee548a00b5a2d81d5de5bc04534af00c874cd62f
ShawnXuan <xiexuanx2@gmail.com>
2017-09-15 21:49:37 +0800

    baseline for SD card

---
+++ ESP32_MPU9250_WIFI.ino
@@ -0,0 +1,200 @@
+/* MPU9250 Basic Example Code
+ by: Kris Winer
+ date: April 1, 2014
+ license: Beerware - Use this code however you'd like. If you
+ find it useful you can buy me a beer some time.
+ Modified by Brent Wilkins July 19, 2016
+
+ Demonstrate basic MPU-9250 functionality including parameterizing the register
+ addresses, initializing the sensor, getting properly scaled accelerometer,
+ gyroscope, and magnetometer data out. Added display functions to allow display
+ to on breadboard monitor. Addition of 9 DoF sensor fusion using open source
+ Madgwick and Mahony filter algorithms. Sketch runs on the 3.3 V 8 MHz Pro Mini
+ and the Teensy 3.1.
+
+ SDA and SCL should have external pull-up resistors (to 3.3V).
+ 10k resistors are on the EMSENSR-9250 breakout board.
+
+ Hardware setup:
+ VDD ---------------------- 3.3V
+ VDDI --------------------- 3.3V
+ SDA ----------------------- 21
+ SCL ----------------------- 22
+ GND ---------------------- GND
+ */
+
+#include "MPU9250.h"
+
+#define AHRS true         // Set to false for basic data read
+#define SerialDebug false  // Set to true to get Serial output for debugging
+
+// Pin definitions
+int intPin = 12;  // These can be changed, 2 and 3 are the Arduinos ext int pins
+int myLed  = 13;  // Set up pin 13 led for toggling
+
+MPU9250 myIMU;
+
+#include <WiFi.h>       // use for ESP32
+
+#define sendInterval 100
+
+//const char* ssid = "TP-LINK_B979AC";
+//const char* password = "wjl13146792866";
+const char* ssid = "NETGEAR26";
+const char* password = "zanywater094";
+//const char* ssid = "ShawnX";
+//const char* password = "86753099";
+// 0831 WiFiUDP Udp;
+
+unsigned int localUdpPort = 4210;  // local port to listen on
+char incomingPacket[256];  // buffer for incoming packets
+WiFiServer server(localUdpPort); // 0831
+
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+bool time_arrive = false;
+int f = 0;
+void IRAM_ATTR onTimer(){
+  time_arrive = true;
+}
+void init_timer(){
+  // Use 1st timer of 4 (counted from zero).
+  // Set 80 divider for prescaler (see ESP32 Technical Reference Manual for more
+  // info).
+  timer = timerBegin(0, 80, true);
+
+  // Attach onTimer function to our timer.
+  timerAttachInterrupt(timer, &onTimer, true);
+
+  // Set alarm to call onTimer function every second (value in microseconds).
+  // Repeat the alarm (third parameter)
+  //timerAlarmWrite(timer, 10000, true);//10000-100Hz-10ms
+  timerAlarmWrite(timer, 1000000, true);//1000000-1Hz-1000ms
+
+  // Start an alarm
+  timerAlarmEnable(timer);
+}
+
+void setup()
+{
+  Wire.begin(21, 22, 400000);
+  // TWBR = 12;  // 400 kbit/sec I2C speed
+  Serial.begin(115200);
+  
+  //Setup wifi and udp connection
+  Serial.println();
+  Serial.printf("Connecting to %s ", ssid);
+    
+  // delete old config
+  WiFi.disconnect(true);
+    
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");// 0831 
+  Serial.println("WiFi connected.");// 0831 
+  Serial.println("IP address: ");// 0831 
+  Serial.println(WiFi.localIP());// 0831 
+  server.begin();// 0831
+   
+  // Set up the interrupt pin, its set as active high, push-pull
+  pinMode(intPin, INPUT);
+  digitalWrite(intPin, LOW);
+  pinMode(myLed, OUTPUT);
+  digitalWrite(myLed, HIGH);
+
+  // Read the WHO_AM_I register, this is a good test of communication
+  byte c = myIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
+  Serial.print("MPU9250 "); Serial.print("I AM "); Serial.print(c, HEX);
+  Serial.print(" I should be 0x73 or"); Serial.println(0x71, HEX);
+
+  if (c == 0x71 or c == 0x73) // WHO_AM_I should always be 0x68
+  {
+    Serial.println("MPU9250 is online...");
+
+    // Start by performing self test and reporting values
+    myIMU.MPU9250SelfTest(myIMU.SelfTest);
+    Serial.print("x-axis self test: acceleration trim within : ");
+    Serial.print(myIMU.SelfTest[0],1); Serial.println("% of factory value");
+    Serial.print("y-axis self test: acceleration trim within : ");
+    Serial.print(myIMU.SelfTest[1],1); Serial.println("% of factory value");
+    Serial.print("z-axis self test: acceleration trim within : ");
+    Serial.print(myIMU.SelfTest[2],1); Serial.println("% of factory value");
+    Serial.print("x-axis self test: gyration trim within : ");
+    Serial.print(myIMU.SelfTest[3],1); Serial.println("% of factory value");
+    Serial.print("y-axis self test: gyration trim within : ");
+    Serial.print(myIMU.SelfTest[4],1); Serial.println("% of factory value");
+    Serial.print("z-axis self test: gyration trim within : ");
+    Serial.print(myIMU.SelfTest[5],1); Serial.println("% of factory value");
+
+    // Calibrate gyro and accelerometers, load biases in bias registers
+    myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
+
+    myIMU.initMPU9250();
+    // Initialize device for active mode read of acclerometer, gyroscope, and
+    // temperature
+    Serial.println("MPU9250 initialized for active data mode....");
+
+    // Read the WHO_AM_I register of the magnetometer, this is a good test of
+    // communication
+    byte d = myIMU.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963);
+    Serial.print("AK8963 "); Serial.print("I AM "); Serial.print(d, HEX);
+    Serial.print(" I should be "); Serial.println(0x48, HEX);
+
+    // Get magnetometer calibration from AK8963 ROM
+    myIMU.initAK8963(myIMU.magCalibration);
+    // Initialize device for active mode read of magnetometer
+    Serial.println("AK8963 initialized for active data mode....");
+    if (SerialDebug)
+    {
+      //  Serial.println("Calibration values: ");
+      Serial.print("X-Axis sensitivity adjustment value ");
+      Serial.println(myIMU.magCalibration[0], 2);
+      Serial.print("Y-Axis sensitivity adjustment value ");
+      Serial.println(myIMU.magCalibration[1], 2);
+      Serial.print("Z-Axis sensitivity adjustment value ");
+      Serial.println(myIMU.magCalibration[2], 2);
+    }
+  } // if (c == 0x71)
+  else
+  {
+    Serial.print("Could not connect to MPU9250: 0x");
+    Serial.println(c, HEX);
+    while(1) ; // Loop forever if communication doesn't happen
+  }
+
+  Serial.println(F("BMP280 test"));
+  if (!myIMU.bme.begin()) {  
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
+  init_timer();
+}
+void TimerArrive(){
+  f++;    
+  if(time_arrive){  
+    Serial.println(f);
+    f = 0;
+    time_arrive = false;
+  }
+}
+void loop()
+{
+  myIMU.update(SerialDebug);
+  TimerArrive();
+  
+  WiFiClient client = server.available(); 
+  if (client) {                     // if you get a client,
+    Serial.println("New Client."); // print a message out the serial port
+    String currentLine = "";        // make a String to hold incoming data from the client
+    while (client.connected()) {    // loop while the client's connected
+      //if(time_arrive){
+        myIMU.update(SerialDebug);
+        myIMU.SendUDPMessage(client);
+        TimerArrive();
+      //}
+    }
+  }
+}
